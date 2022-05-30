8635b1fe51438bfefa0f490e21d4e52cc5375816
youdaoyzbx <youdaoyzbx@163.com>
2022-05-30 18:29:36 +0800

    remove additional comment and log

--- start.py
+++ start.py
@@ -5,7 +5,6 @@ import subprocess
 import sys
 
 import cv2
-from loguru import logger
 from ymir_exc import dataset_reader as dr
 from ymir_exc import env, monitor
 from ymir_exc import result_writer as rw
@@ -18,9 +17,9 @@ from utils.ymir_yolov5 import (YmirYolov5, convert_ymir_to_yolov5,
 def start() -> int:
     env_config = env.get_current_env()
 
-    logger.add(osp.join(env_config.output.root_dir, 'ymir_start.log'))
+    logging.add(osp.join(env_config.output.root_dir, 'ymir_start.log'))
 
-    logger.info(f'env_config: {env_config}')
+    logging.info(f'env_config: {env_config}')
 
     if env_config.run_training:
         _run_training(env_config)
@@ -29,32 +28,33 @@ def start() -> int:
     elif env_config.run_infer:
         _run_infer(env_config)
     else:
-        logger.warning('no task running')
+        logging.warning('no task running')
 
     return 0
 
 
 def _run_training(env_config: env.EnvConfig) -> None:
     """
-    sample function of training, which shows:
-    1. how to get config file
-    2. how to read training and validation datasets
-    3. how to write logs
-    4. how to write training result
+    function for training task
+    1. get merged config from ymir_exc and code_config file
+    2. convert dataset
+    3. training model
+    4. save model weight/hyperparameter/... to design directory
     """
-    # use `env.get_executor_config` to get config file for training
+    # 1. use `env.get_executor_config` to get config file for training
     executor_config = get_universal_config()
 
     # use `logging` or `print` to write log to console
     #   notice that logging.basicConfig is invoked at executor.env
     logging.info(f"training config: {executor_config}")
 
-    logger.info('start convert ymir dataset to yolov5 dataset')
+    # 2. convert dataset
+    logging.info('convert ymir dataset to yolov5 dataset')
     out_dir = osp.join(env_config.output.root_dir, 'yolov5_dataset')
     convert_ymir_to_yolov5(out_dir)
-    logger.info('convert ymir dataset to yolov5 dataset finished!!!')
     monitor.write_monitor_logger(percent=ymir_process_config['preprocess'])
 
+    # 3. training model
     epochs = executor_config.get('epochs', 300)
     batch_size = executor_config.get('batch_size', 64)
     model = executor_config.get('model', 'yolov5s')
@@ -68,77 +68,51 @@ def _run_training(env_config: env.EnvConfig) -> None:
         f'--img-size {img_size} --hyp data/hyps/hyp.scratch-low.yaml ' + \
         '--exist-ok'
     # use `monitor.write_monitor_logger` to write write task process percent to monitor.txt
-    logger.info('start training ' + '*' * 50)
-    logger.info(f'{command}')
-    logger.info('*' * 80)
+    logging.info(f'start training: {command}')
 
     # os.system(command)
     subprocess.check_output(command.split())
     monitor.write_monitor_logger(percent=ymir_process_config['preprocess'] + ymir_process_config['task'])
 
-    # convert to onnx
-    logging.info('convert to onnx ' + '*' * 50)
+    # 4. convert to onnx and save model weight to design directory
     opset = executor_config['opset']
     command = f'python export.py --weights {models_dir}/best.pt --opset {opset} --include onnx'
-    logger.info(f'{command}')
-    logger.info('*' * 80)
-    # os.system(command)
+    logging.info(f'export onnx weight: {command}')
     subprocess.check_output(command.split())
-    # suppose we have a long time training, and have saved the final model
 
+    # save hyperparameter
     shutil.copy(f'models/{model}.yaml', f'{models_dir}/{model}.yaml')
 
     # if task done, write 100% percent log
-    logging.info('convert done')
     monitor.write_monitor_logger(percent=1.0)
 
 
 def _run_mining(env_config: env.EnvConfig) -> None:
-    # use `env.get_executor_config` to get config file for training
-    #   models are transfered in executor_config's model_params_path
-    executor_config = env.get_executor_config()
-    # use `logging` or `print` to write log to console
+    executor_config = get_universal_config()
     logging.info(f"mining config: {executor_config}")
 
-    logger.info('start convert ymir dataset to yolov5 dataset')
+    logging.info('convert ymir dataset to yolov5 dataset')
     out_dir = osp.join(env_config.output.root_dir, 'yolov5_dataset')
     convert_ymir_to_yolov5(out_dir)
-    logger.info('convert ymir dataset to yolov5 dataset finished!!!')
     monitor.write_monitor_logger(percent=ymir_process_config['preprocess'])
 
     command = 'python mining/mining_cald.py'
+    logging.info(f'mining: {command}')
     subprocess.check_output(command.split())
-
-    # write mining result
-    #   here we give a fake score to each assets
-    # total_length = len(asset_paths)
-    # mining_result = [(asset_path, index / total_length) for index, asset_path in enumerate(asset_paths)]
-    # rw.write_mining_result(mining_result=mining_result)
-
-    # if task done, write 100% percent log
-    logging.info('mining done')
     monitor.write_monitor_logger(percent=1.0)
 
 
 def _run_infer(env_config: env.EnvConfig) -> None:
-    # use `env.get_executor_config` to get config file for training
-    #   models are transfered in executor_config's model_params_path
-    executor_config = env.get_executor_config()
-
-    # use `logging` or `print` to write log to console
+    executor_config = get_universal_config()
     logging.info(f"infer config: {executor_config}")
 
     # generate data.yaml for infer
-    logger.info('start convert ymir dataset to yolov5 dataset')
+    logging.info('convert ymir dataset to yolov5 dataset')
     out_dir = osp.join(env_config.output.root_dir, 'yolov5_dataset')
     convert_ymir_to_yolov5(out_dir)
-    logger.info('convert ymir dataset to yolov5 dataset finished!!!')
     monitor.write_monitor_logger(percent=ymir_process_config['preprocess'])
 
-    # use `monitor.write_monitor_logger` to write log to console and write task process percent to monitor.txt
     N = dr.items_count(env.DatasetType.CANDIDATE)
-    logging.info(f"assets count: {N}")
-
     infer_result = dict()
     model = YmirYolov5()
     idx = 0
@@ -152,13 +126,7 @@ def _run_infer(env_config: env.EnvConfig) -> None:
         idx += 1
         monitor.write_monitor_logger(percent=ymir_process_config['preprocess'] + ymir_process_config['task'] * idx / N)
 
-    # write infer result
-    # fake_annotation = rw.Annotation(class_name=class_names[0], score=0.9, box=rw.Box(x=50, y=50, w=150, h=150))
-    # infer_result = {asset_path: [fake_annotation] for asset_path in asset_paths}
     rw.write_infer_result(infer_result=infer_result)
-
-    # if task done, write 100% percent log
-    logging.info('infer done')
     monitor.write_monitor_logger(percent=1.0)
 
 
